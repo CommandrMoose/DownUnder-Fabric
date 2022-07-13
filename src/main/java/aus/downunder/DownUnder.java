1eb4f140f3dbcf0c325e73bc81421c6439bf208e
Clayton Trewben <34910888+CommandrMoose@users.noreply.github.com>
2022-07-14 09:02:19 +1000

    Entrypoint setup

---
+++ src/main/java/aus/downunder/DownUnder.java
@@ -0,0 +1,13 @@
+package aus.downunder;
+
+import net.fabricmc.api.ModInitializer;
+
+public class DownUnder implements ModInitializer {
+
+    public static final String MODID = "downunder";
+
+    @Override
+    public void onInitialize() {
+
+    }
+}
